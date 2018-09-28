6efe00fad99160e82d50ae3d8dede79566819691
Adomix <33987862+Adomix@users.noreply.github.com>
2018-09-28 10:26:19 -0400

    Added PBClient (I did this on mobile rip)

---
+++ PBClient.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PushBulletNet
+{
+    public class PBClient
+    {
+    
+        internal static string token;
+
+        public PBClient(string Token)
+        {
+            token = Token;
+        }
+    
+    }
+}
