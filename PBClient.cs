4ab297377ff2817d942077af9b5e6683196d5b52
Adomix <33987862+Adomix@users.noreply.github.com>
2018-09-28 11:43:22 -0400

    Update PBClient.cs

--- PBClient.cs
+++ PBClient.cs
@@ -6,13 +6,11 @@ namespace PushBulletNet
 {
     public class PBClient
     {
-    
         internal static string token;
-
+        
         public PBClient(string Token)
         {
             token = Token;
         }
-    
     }
 }
